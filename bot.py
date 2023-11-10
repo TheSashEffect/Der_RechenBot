042c1ba16313d3af23cd967221d10559d6b27252
TheSashEffect <sascharosner.2011@gmail.com>
2023-11-10 18:44:24 +0100

    updated code

--- bot.py
+++ bot.py
@@ -519,10 +519,7 @@ async def temp(ctx):
 
 @bot.command(name='download', help='Download a file and save it to the Raspberry Pi')
 async def download(ctx, file_url):
-    if ctx.author.id in (726079395974086680):
-        # Get the author of the message (user who sent the command)
-        author = ctx.message.author
-
+    if ctx.author.id == 726079395974086680:
         # Specify the directory where the file will be saved
         save_directory = '/mnt/drive/download'
 
